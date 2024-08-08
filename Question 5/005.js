e2ec18c7353cea51dec44e06096fdde185a41859
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 21:55:41 +0500

    Add files via upload

---
+++ Question 5/005.js
@@ -0,0 +1,6 @@
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a 
+//variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famousPerson = "Quaid e Azam";
+var famousQoute = "' Idont belive in talking the right decision, I take decision and make it right.";
+var massige = "".concat(famousPerson, " said , ").concat(famousQoute);
+console.log(massige);
---
+++ Question 5/005.ts
@@ -0,0 +1,8 @@
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a 
+//variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+let famousPerson: string = "Quaid e Azam";
+let famousQoute: string = "' Idont belive in talking the right decision, I take decision and make it right.";
+let massige: string = `${famousPerson} said , ${famousQoute}`;
+console.log(massige);
